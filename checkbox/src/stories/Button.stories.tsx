56295652f8519f1d9572903eb97c5899cc115990
Isabel Tegström <tegstromisabel@gmail.com>
2026-04-20 14:53:34 +0200

    Storybook buttons

--- checkbox/src/stories/Button.stories.tsx
+++ checkbox/src/stories/Button.stories.tsx
@@ -10,21 +10,54 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const PrimaryDefault: Story = {
   args: {
-    "label": "label"
+    "label": "Continue",
+    primary: true,
+    backgroundColor: "#0F172A",
+    size: "medium"
   }
 }
-export const Hover: Story = {
+export const PrimaryHover: Story = {
   args: {
-    "label": "label"
+    "label": "Continue",
+    primary: true,
+    size: "medium",
+    backgroundColor: "#334155"
+  }
+}
+
+export const DestructiveDefault: Story = {
+  args: {
+    "label": "Destructive",
+    primary: true,
+    backgroundColor: "#EF4444",
+    size: "large"
+  }
+}
+
+export const DestructiveHover: Story = {
+  args: {
+    "label": "Destructive",
+    backgroundColor: "#DC2626",
+    primary: true,
+    size: "large"
+  }
+}
+
+export const OutlineDefault: Story = {
+  args: {
+    "label": "Cancel",
+    primary: true,
+    backgroundColor: "#FFFFFF"
+  }
+}
+
+export const OutlineHover: Story = {
+  args: {
+    "label": "Cancel",
+    backgroundColor: "#F1F5F9",
+    primary: true
   }
 
 }
-
-export const Primary : Story = {
-  args: {
-    "label": "label"
-  }
-
-};
